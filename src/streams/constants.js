2d5b78815e88783ce34354c4584490a5fcac08d6
@shadowinhaze <shadowinhaze@gmail.com>
2022-06-02 20:42:55 +0200

    feat: implement write.js

--- src/streams/constants.js
+++ src/streams/constants.js
@@ -2,3 +2,8 @@ export const ReadStreamModuleConstant = {
   dirName: 'files',
   fileName: 'fileToRead.txt',
 };
+
+export const WriteStreamModuleConstant = {
+  dirName: 'files',
+  fileName: 'fileToWrite.txt',
+};
