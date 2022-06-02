cdf9b057a1a2a83b7827c19257e95f288eddcad5
@shadowinhaze <shadowinhaze@gmail.com>
2022-06-03 00:53:20 +0200

    feat: implement decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,30 @@
+import { join } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createUnzip } from 'zlib';
+import { getDirname } from '../utils/get-dirname.js';
+import { CompressModuleConstant } from './constants.js';
+
 export const decompress = async () => {
-    // Write your code here 
+  const gzip = createUnzip();
+
+  const __dirname = getDirname(import.meta.url);
+
+  const filePath = join(
+    __dirname,
+    CompressModuleConstant.dirName,
+    CompressModuleConstant.compressedFileName,
+  );
+
+  const decompressedFileName = join(
+    __dirname,
+    CompressModuleConstant.dirName,
+    CompressModuleConstant.fileName,
+  );
+
+  const readStream = createReadStream(filePath);
+  const writeStream = createWriteStream(decompressedFileName);
+
+  readStream.pipe(gzip).pipe(writeStream);
 };
+
+decompress();
