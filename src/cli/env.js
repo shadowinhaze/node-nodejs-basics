83078132a82edf995568d83585b8c27a55d3a251
@shadowinhaze <shadowinhaze@gmail.com>
2022-06-02 15:37:30 +0200

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,17 @@
+import { ParseEnvModuleConstants } from './constants.js';
+
 export const parseEnv = () => {
-    // Write your code here 
+  const envData = process.env;
+
+  const envInfo = Object.entries(envData).reduce((resultStr, [key, value]) => {
+    if (key.includes(ParseEnvModuleConstants.envPrefix)) {
+      return `${resultStr} ${key} = ${value};`;
+    }
+
+    return resultStr;
+  }, '');
+
+  console.log(envInfo.trim());
 };
+
+parseEnv();
