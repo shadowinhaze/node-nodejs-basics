3156de29936acf9ba0294236d85910389363cf04
@shadowinhaze <shadowinhaze@gmail.com>
2022-06-02 07:55:58 +0200

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,21 @@
+import { writeFile } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filesDir = path.join(__dirname, '/files');
+
 export const create = async () => {
-    // Write your code here 
+  writeFile(
+    `${filesDir}/fresh.txt`,
+    'I am fresh and young',
+    { flag: 'wx' },
+    (err) => {
+      if (err) throw new Error('FS operation failed');
+    },
+  );
 };
+
+create();
