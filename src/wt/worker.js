9b98bf6ff9a598bbdabd57590fdaba65c29300fc
@shadowinhaze <shadowinhaze@gmail.com>
2022-06-04 10:20:07 +0200

    feat: implement worker_threads task

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,18 @@
+import { workerData, BroadcastChannel, threadId } from 'worker_threads';
+import { PerfCalcModuleConstants } from './constants.js';
+
+const bc = new BroadcastChannel(
+  PerfCalcModuleConstants.mainBroadcastChannelName,
+);
 // n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // This function sends result of nthFibonacci computations to main thread
+  const result = nthFibonacci(workerData);
+
+  bc.postMessage({ threadId, result });
 };
+
+sendResult();
